8dc5a97d11afb509c254c3a7773bed0588edc11a
jackson.magnabosco <jacksonmagnabosco@hotmail.com>
2019-08-09 14:10:27 -0300

    atualização dia 09/08/2019

---
+++ lib/tabs/locker_tab.dart
@@ -0,0 +1,24 @@
+import 'package:armarios_inteligentes/tiles/locker_tile.dart';
+import 'package:armarios_inteligentes/tiles/place_tile.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+
+class PlacesTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<QuerySnapshot>(
+      future: Firestore.instance.collection("lockers").getDocuments(),
+      builder: (context, snapshot){
+        if(!snapshot.hasData)
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        else
+          return ListView(
+            children: snapshot.data.documents.map((doc) => LockerTile(doc)).toList(),
+          );
+      },
+    );
+  }
+}
---
+++ lib/tiles/locker_tile.dart
@@ -0,0 +1,24 @@
+import 'package:armarios_inteligentes/screens/category_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+
+class LockerTile extends StatelessWidget {
+
+  final DocumentSnapshot snapshot;
+
+  LockerTile(this.snapshot);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(snapshot.data["QR Code"]),
+      trailing: Icon(Icons.keyboard_arrow_right),
+      onTap: (){
+        Navigator.of(context).push(
+            MaterialPageRoute(builder: (context)=>CategoryScreen(snapshot))
+        );
+      },
+    );
+  }
+}
